588bdeeaa95929bbce8f99639a523ebf29aa3f44
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2021-01-04 14:15:02 -0500

    Create script.java
    - match class names
    - understand whats going on

---
+++ script.java
@@ -0,0 +1,15 @@
+
+            const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper');
+           
+            portfolioItems.forEach(portfolioItem => {
+                portfolioItem.addEventListener('mouseover', () => {
+                    console.log(portfolioItem.childNodes[1].classList)
+                    portfolioItem.childNodes[1].classList.add('img-blur');
+                });
+        
+                portfolioItem.addEventListener('mouseout', () => {
+                    console.log(portfolioItem.childNodes[1].classList)
+                    portfolioItem.childNodes[1].classList.remove('img-blur');
+                });
+        
+            });
